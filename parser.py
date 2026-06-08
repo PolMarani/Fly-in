ee115bfc3af18196d6f5a6b2cf4c81a120eb0741
Paolo Marani <pmarani@c2r5p1.42firenze.it>
2026-06-08 22:07:03 +0200

    lel

--- parser.py
+++ parser.py
@@ -1,4 +1,7 @@
+from sys import argv
+
 class Zone:
+    
     def __init__(self, name: str, x: int, y: int, 
                  zone_type: str = "normal",
                  max_drones: int = 1,
@@ -12,12 +15,39 @@ class Zone:
 
 
 class Connection:
+
     def __init__(self, zone1: str, zone2: str,
                  max_link_capacity: int = 1):
         self.zone1 = zone1
         self.zone2 = zone2
         self.max_link_capacity = max_link_capacity
+        map = {}
 
 
 class Parser:
-    
+
+    def __init__(self):
+        map_file = argv[1];
+        self.read_map(map_file)
+
+    def read_map(self, map_file: str):
+        with open(map_file, "r") as file:
+            for line in file:
+
+                if line.startswith('#'):
+                    continue
+
+                if line.startswith("start_hub"):
+                    attributes = line.split()
+                    stripped_attr = attributes[4].strip("[]")
+                    splitted_attr = stripped_attr.split(" ")
+                    for element in splitted_attr:
+                        if element[0] == "color"
+
+
+                elif line.startswith("end_hub"):
+                    
+                elif line.startswith('hub'):
+                    
+                elif line.startswith('connection'):
+                    
