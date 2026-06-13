5f439f854dfb17d997cfd1ca799f2486a2a64b5f
PolMarani <marani.paolo99@gmail.com>
2026-06-13 18:16:34 +0200

    lele

--- pathfinder.py
+++ pathfinder.py
@@ -1,5 +1,25 @@
-from parser import Zone, Connection
+from map_parser import Zone, Connection, Parser
+import heapq
+
 
 class Pathfinder:
-    def __init__(self, zones: dict[str, Zone]):
-        pass
+    def __init__(self,
+                 zones: dict[str, Zone],
+                 connections: dict[str, Connection],
+                 nb_drones: int,
+                 start_hub: Zone,
+                 end_hub: Zone):
+        self.nb_drones = nb_drones
+        self.zones = zones
+        self.connections = connections
+        self.costs = {}
+        self.queue = []
+
+    def find_path(self) -> list[Zone | Connection]:
+        self.costs = {element: float("inf") for element in self.zones}
+        self.costs[self.start_hub.name] = 0
+        heapq.heappush(self.queue, (0, self.start_hub.name))
+
+        while self.queue:
+            costo, zona = heapq.heappop(self.queue)
+            
